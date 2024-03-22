76a7d0e3df09d7e7dd5d77275f8e9a6a85f69f13
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-22 13:51:16 +0100

    Sets a random color on hover and lets trail

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ for (i = 0; i < 256; i++) {
     gridItem.setAttribute('class', 'gridItem');
     container.appendChild(gridItem);
 }
+
+function randColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r} ${g} ${b})`;
+}
+
+let gridItems = document.querySelectorAll('.gridItem');
+
+for (const gridItem of gridItems) {
+    gridItem.addEventListener('mouseenter', () => {
+        gridItem.style.backgroundColor = randColor();
+    })
+    gridItem.addEventListener('mouseleave', () => {
+        gridItem.style.backgroundColor = '';
+    })
+}
